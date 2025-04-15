5c98a61563b52f1810e0ca7b88962538066fcd6b
pedroKleinDavila <p.klein@edu.pucrs.br>
2025-04-15 11:06:33 -0300

    primeira versão

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+int main() {
+    FILE *fp;
+    fp = fopen("imagem.ppm","r");
+    char formato[3];
+    fscanf(fp, "%s", formato);
+    printf("%s\n", formato);
+    int coluna, linha;
+    fscanf(fp, "%d %d", &coluna, &linha); 
+    printf("%d %d\n", coluna, linha);
+    int valor;
+    fscanf(fp, "%d", &valor); 
+    printf("%d\n", valor);
+    int i, j;
+    int r, g, b;
+
+	for(j=0; j<linha; j++)
+	{
+		for(i=0; i<coluna; i++)
+		{  	
+  			fscanf(fp, "%d %d %d", &r, &g, &b);
+		  	printf("%d %d %d\n", r, g, b);
+		}
+	}
+    fclose(fp);
+
+    return 0;
+}
